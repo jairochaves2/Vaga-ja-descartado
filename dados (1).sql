71f2bb41a8246f24987356699963855fe7892b47
ericvini <39422976+ericvini@users.noreply.github.com>
2018-05-19 09:41:57 -0300

    Add files via upload

---
+++ dados (1).sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 19-Maio-2018 às 01:55
+-- Versão do servidor: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cadastro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `dados`
+--
+
+CREATE TABLE IF NOT EXISTS `dados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(220) NOT NULL,
+  `cpf` varchar(220) NOT NULL,
+  `email` varchar(220) NOT NULL,
+  `telefone` varchar(50) NOT NULL,
+  `endereço` varchar(150) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `site` varchar(100) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  `tipo_oportunidade` varchar(220) NOT NULL,
+  `cargo` varchar(220) NOT NULL,
+  `descrição` text NOT NULL,
+  `área_atuação` varchar(50) NOT NULL,
+  `perfil_candidato` text NOT NULL,
+  `numero_vagas` int(255) NOT NULL,
+  `cidade_vaga` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
