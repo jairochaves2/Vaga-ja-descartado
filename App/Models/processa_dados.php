14a0e452af5265a8194ce04e6ac2f1f0661d1d7c
idelfrides <idelfridesjorge@alu.ufc.br>
2018-05-21 14:47:37 -0300

    new project organization

---
+++ App/Models/processa_dados.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Idelfides Jorge, WM
+ * Date: 19/05/2018
+ * Time: 20:00
+ *
+ * ----------------------------------------------------------------
+ * Este arquivo irá conter as funções necessários para tratar dados vindos
+ * de formulários
+ */
+
+    /*  métodos definidos para gerenciamento de  vagas */
+    // ofertar vaga
+    function cadastroVaga(){
+        echo "obama";
+
+    }
+
+    function listVagas(){
+
+    }
+
+    function findData2table(){
+
+    }
+
+    //atualiza dados de vaga
+    function updateVaga(){
+
+    }
+
+    function removeVaga(){
+
+    }
+
+    /* -------------------------------------------------------------
+        métodos definidos para gerenciamento de  vagas
+    ---------------------------------------------------------------*/
+    // cadastro de curriculo pra concorrer a uma vaga
+    function cadastrarCurriculo(){
+
+    }
+
